8856eea0a762d1630ac0c979aa8d964f0081d88d
thewritecoder <thewritecoder@gmail.com>
2020-08-13 15:14:42 +1000

    Adding cosole logging on viewport resize.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,6 @@
-const updateViewportHeight = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+const updateViewportHeight = () => {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+    console.log("Setting VH to " + `${window.innerHeight}px`);
+}
 window.addEventListener('resize', updateViewportHeight)
 updateViewportHeight();
