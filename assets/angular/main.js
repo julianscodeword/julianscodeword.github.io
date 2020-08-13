afbf35d9ce72c58ed5027881ca32c9da6f5a367b
thewritecoder <thewritecoder@gmail.com>
2020-08-14 08:46:03 +1000

    Added logging on mouse events.

--- assets/angular/main.js
+++ assets/angular/main.js
@@ -55013,18 +55013,9 @@ let PuzzleComponent = /*@__PURE__*/ (() => {
             this.isFinished = false;
             this.onWordClicked = new _angular_core__WEBPACK_IMPORTED_MODULE_0__["EventEmitter"]();
         }
-        mouseOver(event) {
-            event.stopPropagation();
-            this.isActive = true;
-            this.setNavigating(true);
-            return false;
-        }
-        mouseOut(event) {
-            event.stopPropagation();
-            this.isActive = false;
-            this.setNavigating(false);
-            return false;
-        }
+        mouseEvents(event) { console.log(event); }
+        mouseOver() { this.isActive = true; this.setNavigating(true); }
+        mouseOut() { this.isActive = false; this.setNavigating(false); }
         get isIntrusive() { return this.appearance.isIntrusive; }
         setNavigating(isNavigating) {
             if (isNavigating)
@@ -55045,7 +55036,7 @@ let PuzzleComponent = /*@__PURE__*/ (() => {
     PuzzleComponent.ɵfac = function PuzzleComponent_Factory(t) { return new (t || PuzzleComponent)(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["DomSanitizer"]), _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_core__WEBPACK_IMPORTED_MODULE_0__["Renderer2"])); };
     PuzzleComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({ type: PuzzleComponent, selectors: [["x-puzzle"]], hostVars: 10, hostBindings: function PuzzleComponent_HostBindings(rf, ctx) {
             if (rf & 1) {
-                _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("mouseover", function PuzzleComponent_mouseover_HostBindingHandler($event) { return ctx.mouseOver($event); })("mouseout", function PuzzleComponent_mouseout_HostBindingHandler($event) { return ctx.mouseOut($event); });
+                _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("touchstart", function PuzzleComponent_touchstart_HostBindingHandler($event) { return ctx.mouseEvents($event); })("click", function PuzzleComponent_click_HostBindingHandler($event) { return ctx.mouseEvents($event); })("hover", function PuzzleComponent_hover_HostBindingHandler($event) { return ctx.mouseEvents($event); })("mouseover", function PuzzleComponent_mouseover_HostBindingHandler() { return ctx.mouseOver(); })("mouseout", function PuzzleComponent_mouseout_HostBindingHandler() { return ctx.mouseOut(); });
             }
             if (rf & 2) {
                 _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵstyleMap"](ctx.style);
