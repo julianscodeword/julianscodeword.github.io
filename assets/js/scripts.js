8d246be48a8a9caf162ae951e197920ba0e09c20
thewritecoder <thewritecoder@gmail.com>
2020-08-13 17:06:54 +1000

    Added logging for viewport height updates.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,8 @@
-const updateViewportHeight = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+const updateViewportHeight = () => {
+    const height = `${window.innerHeight}px`;
+    console.log(`BEFORE Setting height to ${height}.`);
+    document.documentElement.style.setProperty('--vh', height);
+    console.log(`AFTER Setting height to ${height}.`);
+}
 window.addEventListener('resize', updateViewportHeight)
 updateViewportHeight();
