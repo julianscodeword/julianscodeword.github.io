40aa8ec0aa25231065aa4809750ca7b2f9c29892
thewritecoder <thewritecoder@gmail.com>
2020-08-13 15:16:31 +1000

    Changed log to alert.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 const updateViewportHeight = () => {
     document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
-    console.log("Setting VH to " + `${window.innerHeight}px`);
+    alert("Setting VH to " + `${window.innerHeight}px`);
 }
 window.addEventListener('resize', updateViewportHeight)
 updateViewportHeight();
