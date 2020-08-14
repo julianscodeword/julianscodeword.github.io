d85f5156dcc9337165506c9a219525ca3ac4d724
thewritecoder <thewritecoder@gmail.com>
2020-08-15 07:33:36 +1000

    Remove logging on viewport change.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,3 @@
-const updateViewportHeight = () => {
-    const height = `${window.innerHeight}px`;
-    console.log(`BEFORE Setting height to ${height}.`);
-    document.documentElement.style.setProperty('--vh', height);
-    console.log(`AFTER Setting height to ${height}.`);
-}
+const updateViewportHeight = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
 window.addEventListener('resize', updateViewportHeight)
 updateViewportHeight();
