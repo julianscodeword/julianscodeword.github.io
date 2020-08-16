aed13819c109f62f7b149b53e218253df4cdb1d8
thewritecoder <thewritecoder@gmail.com>
2020-08-16 11:35:47 +1000

    Adding all viewport dimensions as css variables.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,8 @@
-const updateViewportHeight = () => document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+const updateViewportHeight = () => {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+    document.documentElement.style.setProperty('--vw', `${window.innerWidth}px`);
+    document.documentElement.style.setProperty('--vmin', `${Math.min(window.innerHeight, window.innerWidth)}px`);
+    document.documentElement.style.setProperty('--vmax', `${Math.max(window.innerHeight, window.innerWidth)}px`);
+}
 window.addEventListener('resize', updateViewportHeight)
 updateViewportHeight();
